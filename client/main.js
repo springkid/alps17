9c7f407d68f31569091818f66e0763a51bbe840a
erisso <works@erisso.com>
2016-10-04 13:04:20 +0200

    API de discogs added en server

--- client/main.js
+++ client/main.js
@@ -2,7 +2,3 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
-// Instantiation
-var secretDiscogsAccessCode = Meteor.settings.consumerKey;
-const discogs = new Discogs();
--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// code to run on server at startup
+	// Instantiation of Discogs API keys
+	var secretDiscogsAccessCode = Meteor.settings.discogs.consumerKey;
+	const discogs = new Discogs();
+	search = discogs.search('foo fighters');
+	console.log('Searched for Foo Fighters' + search);
+
 });
