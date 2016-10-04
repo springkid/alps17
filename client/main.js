aeb32a8c986aae2c81c01264244100ccc2e1ee04
erisso <works@erisso.com>
2016-10-04 16:04:13 +0200

    Estructura de la aplicación, Templates, datos

--- client/main.js
+++ client/main.js
@@ -2,3 +2,13 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
+
+/* Helpers */ 
+Template.artistListElement.helpers({
+  artists: [
+    { name: 'Shellac', addedBy: '{Pere, Jose}', confirmedTrue: 'No' },
+    { name: 'Triangulo de Amor Bizarro', addedBy: '{Jose}', confirmedTrue: 'No' },
+    { name: 'Metallica', addedBy: 'Jose', confirmedTrue: 'No' },
+  ],
+});
--- server/main.js
+++ server/main.js
@@ -5,7 +5,8 @@ Meteor.startup(() => {
 	// Instantiation of Discogs API keys
 	var secretDiscogsAccessCode = Meteor.settings.discogs.consumerKey;
 	const discogs = new Discogs();
-	search = discogs.search('foo fighters');
-	console.log('Searched for Foo Fighters' + search);
+	search = discogs.search('foo fighters there is nothing left to loose');
+	console.log('Searched for Foo Fighters');
+	console.log(search);
 
 });
