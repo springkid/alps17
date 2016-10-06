d2e58b2c542f2e092f565230d4eaed61c61929f9
erisso <works@erisso.com>
2016-10-06 18:22:03 +0200

    Peleando con la API y el form para añadir artists

--- client/main.js
+++ client/main.js
@@ -16,14 +16,27 @@ Template.artistListElement.helpers({
 
 // EVENTS
 Template.searchContent.events({
-	"click #js-search-discogs": function(event){
-		console.log('clicked submit de search');
+	"submit #new-artist": function(event){
+		event.preventDefault();
+		
+	    // Get value from form element
+	    const target = event.target;
+	    console.log(target.searchText.value);
+
 		// Buscar en discogs a través de la API. 
 		// Como está definido todo en un server block, lo hacemos con un Meteor.call
-		Meteor.call("searchDiscogs",function(err, res) {
-			console.log(res);
-			console.log('Searched for Foo Fighters from the client');
-		});
+/*		Meteor.call("searchDiscogs",function(err, res) {
+			if (!err) {
+				console.log(res);
+				return res;
+			}
+			else {
+				console.log('error in Search');
+			}
+		});*/
+
+    	// Clear form
+    	target.searchText.value = '';
 
 	}
 })
